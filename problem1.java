e8fd050cdb5bdb5ada7dfdc3696fd2b074bd02b0
khangv9342 <kmvo100@student.hbuhsd.edu>
2024-09-04 10:11:48 -0700

    rgbewgr

--- problem1.java
+++ problem1.java
@@ -1,1 +1,13 @@
 // Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        String favfood;
+        favfood = "potatos";
+        
+        
+        System.out.println(favfood);
+        System.out.print("I like to eat " + favfood + " as well"); 
+    }
+}

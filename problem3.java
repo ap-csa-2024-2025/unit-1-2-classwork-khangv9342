8c8d5607e10fe60087682d9e2684bda05759cc56
khangv9342 <kmvo100@student.hbuhsd.edu>
2024-09-04 10:21:37 -0700

    efcqefcqcfewqf

--- problem3.java
+++ problem3.java
@@ -1,1 +1,16 @@
 // Create your class here; don't forget the main method
+public class problem3
+{
+    public static void main(String[] args)
+    {
+        String Name;
+        String Location;
+
+        Name = "Khang";
+        Location = "California";
+        
+        
+        
+        System.out.print("I like to eat " + favfood + " as well"); 
+    }
+}

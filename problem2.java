898d3770a5d813c2379beb11e3ddfba4f7c6f700
khangv9342 <kmvo100@student.hbuhsd.edu>
2024-09-04 10:19:15 -0700

    eklwfhcnuiqfcwqgfnweqwuiegfc

--- problem2.java
+++ problem2.java
@@ -1,1 +1,19 @@
 // Create your class here; don't forget the main method
+public class problem2
+{
+    public static void main(String[] args)
+    {
+        String name1;
+        String name2;
+        String name3;
+        String name4;
+
+        name1 = "Sandy";
+        name2 = "David";
+        name3 = "Elaine";
+        name4 = "Jesse";
+
+        System.out.print(name1 + " " + name2 + " " + name3 + " " + name4); 
+        
+    }
+}
